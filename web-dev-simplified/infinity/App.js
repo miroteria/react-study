9ed8018313fed04c55fa4c3e46a58bb6d557f7de
miroteria <miroslav.simi3@gmail.com>
2025-10-19 16:56:07 +0200

    Add App component for book search functionality

---
+++ web-dev-simplified/infinity/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import useBookSearch from './useBookSearch';
+
+function App() {
+  const [ query, setQuery ] = useState('')
+  const [ pageNumber, setPageNumber ] = useState(1)
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  useBookSearch(query, pageNumber)
+  return (
+    <>
+      <input type="text" onChange={handleSearch}></input>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading...</div>
+      <div>Error</div>
+    </>
+  )
+}
+
+export default App;
