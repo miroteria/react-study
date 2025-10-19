e21feeea28fa4483ed06aaab81db90fa10331a19
miroteria <miroslav.simi3@gmail.com>
2025-10-19 17:32:15 +0200

    Enhance useBookSearch to return loading state and books

--- web-dev-simplified/infinity/useBookSearch.js
+++ web-dev-simplified/infinity/useBookSearch.js
@@ -8,6 +8,8 @@ export default function useBookSearch(query, pageNumber) {
     const [hasMore, setHasMore] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
+        setError(false)
         let cancel
         axios({
             method: 'GET',
@@ -15,11 +17,17 @@ export default function useBookSearch(query, pageNumber) {
             params: { q:query, page: pageNumber },
             cancelToken: new axios.CancelToken(c => cancel = c)
         }).then(res => {
+            setBooks(prevBooks => {
+                return [...new Set([...prevBooks, res.data.docs.map(b => b.title)])]
+            })
+            setHasMore(res.data.docs.length > 0)
+            setLoading(false)
             console.log(res.data)
         }).catch(e => {
             if (axios.isCancel(e)) return
+            setError(true)
         })
         return () => cancel()
     }, [query, pageNumber])
-  return null
+  return { loading, error, books, hasMore}
 }
